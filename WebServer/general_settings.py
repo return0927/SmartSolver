bc2f8e336a61d11ee0c41502c6d06af396fc61a7
R3turn0927 <bjleh0927@gmail.com>
2017-11-03 02:00:10 +0900

    서버 기본 설정파일

---
+++ WebServer/general_settings.py
@@ -0,0 +1,13 @@
+import htmls
+
+class Settings:
+    def __init__(self):
+        self.host = "0.0.0.0"
+        self.port = 80
+
+        self.html = htmls.Html()
+
+        self.hostKey = "YOUR KEY HERE"
+
+        self.rootDir = __import__("os").getcwd()
+        self.htmlDir = self.rootDir+"/html/"
