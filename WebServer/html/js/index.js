7ddc7c344f20a9c3f6f461d3a68304ba7d58919c
R3turn0927 <bjleh0927@gmail.com>
2017-11-15 20:13:49 +0900

    메인페이지 Books,Years,Curriculumns,Publishers 처리

--- WebServer/html/js/index.js
+++ WebServer/html/js/index.js
@@ -7,9 +7,62 @@ window.toggle = function (elem) {
 	hidden_menu.style.opacity = opened ? 0 : 1;
 };
 
-function getBooks(elem){
-	var data = elem.value;
-	$.get("/api/textbooks?key=bookname&curr="+data, function(data){
-	    console.log(JSON.parse(data).result);
-    })
+function setBooks(){
+	var data = $("select#curr")[0].value;
+	$.get("/api/textbooks?key=bookname&curr="+data, function(response){_setBooks(JSON.parse(response).result);});
+}
+
+function _setBooks(arr) {
+	var book = $("select#book")[0], i;
+	book.length = 0;
+
+	for(i=0;i<arr.length;i++){
+		var data = arr[i];
+
+		var elem = document.createElement("option");
+		elem.value = data;
+		elem.innerHTML = data;
+		book.add(elem, null);
+	}
+
+	/* Iterable YearInfo */
+	setYears();
+
+	return true;
+}
+
+
+function setYears(){
+	var curr = $("select#curr")[0].value;
+	var book = $("select#book")[0].value;
+	$.get("/api/bookModifiedYear?key=bookname&curr="+curr+"&book="+book, function(response){_setYears(JSON.parse(response).result);});
+}
+
+function _setYears(arr) {
+	var book = $("select#year")[0], i;
+	book.length = 0;
+
+	for(i=0;i<arr.length;i++){
+		var data = arr[i];
+
+		var elem = document.createElement("option");
+		elem.value = data.toString();
+		elem.innerHTML = data.toString();
+		book.add(elem, null);
+	}
+	return true;
+}
+
+
+function setBookPublisher(){
+	var curr = $("select#curr")[0].value;
+	var book = $("select#book")[0].value;
+	$.get("/api/bookPublisher?key=bookname&curr="+curr+"&book="+book, function(response){_setBookPublisher(JSON.parse(response).result);});
+}
+
+function _setBookPublisher(data) {
+	var pub = $("#bookPublisher")[0];
+	pub.innerHTML = "출판사: <font color='#8b0000'>"+data+"</font>";
+
+	return true;
 }
