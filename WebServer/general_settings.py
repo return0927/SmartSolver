93c03bc2ab8d8355dd57325848e3837debc313f4
R3turn0927 <bjleh0927@gmail.com>
2018-01-11 20:51:18 +0900

    이상한짓 패치

--- WebServer/general_settings.py
+++ WebServer/general_settings.py
@@ -2,8 +2,9 @@ import htmls
 
 class Settings:
     def __init__(self):
-        self.host = "0.0.0.0"
-        self.port = 80
+        #Web
+        self.webhost = "0.0.0.0"
+        self.webport = 80
 
         self.html = htmls.Html()
 
@@ -13,3 +14,13 @@ class Settings:
         self.htmlDir = self.rootDir+"/html/"
 
         self.admins = ["bc1916", "sunsky"]
+
+        # UX
+        self.question_cost = 100
+
+        # DB
+        self.host = "localhost"
+        self.port = 5432
+        self.user = "root"
+        self.pw = "SmartSolve2017!@#"
+        self.db = "ssolve"
